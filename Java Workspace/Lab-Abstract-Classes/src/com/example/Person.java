5fb815de68bdfa4d2332a96fa51e9cb66e144bef
AR15614 <edwin.clements@revature.com>
2021-07-20 23:41:11 -0400

    Assignments for 7-20-2021

---
+++ Java Workspace/Lab-Abstract-Classes/src/com/example/Person.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public abstract class Person {
+	
+	protected String name;
+
+    public abstract String getName();
+
+    public abstract void setName(String name);
+}
+	
+	
+	
+
+
